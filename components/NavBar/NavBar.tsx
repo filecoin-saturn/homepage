8fd170956166b8f5a383462db9580fa812950c23
Nadiem <nadiem8@msn.com>
2022-03-18 18:04:24 +0100

    Add blur to NavBar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -2,31 +2,57 @@ import { useState } from "react"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import Menu from "../Menu/Menu"
+import { useEffect } from "react"
 
 export default function NavBar() {
     const [isOpen, setIsOpen] = useState(false)
     const path = useRouter()
+    const [isScrolled, setIsScrolled] = useState(false)
+
 
     const linkArray = [
         {title: "How it works", href: "/#howitworks" },
         {title: "Download", href: "/#download" }
     ]
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0
+        }
+        const callback = (entries: IntersectionObserverEntry[]) => {
+            entries.forEach((entry) => {
+                if(entry.target.id === "navbar-area") {
+                    setIsScrolled(!entry.isIntersecting)
+                }
+            });
+          };
+        const observer = new IntersectionObserver(callback, options);
+        const target = document.querySelector('#navbar-area');
+        if(target) observer.observe(target);
+        return () => {
+            if(target) observer.unobserve(target)
+        }
+    }, [])
     
     return (
         <>
             <div className={`md:hidden fixed inset-0 bg-white z-20 transition-transform duration-300  ${isOpen ? `` : `translate-x-full`}`}>
                 <Menu setIsOpen={setIsOpen}/>
             </div>
-            <div id="navbar" className="fixed inset-x-0 z-10">
+            <div id="navbar" className={`fixed inset-x-0 z-10 bg-white ${isScrolled ? `bg-opacity-70 backdrop-blur-md` : ``}`}>
                 <div className="flex justify-between mx-4 md:mx-6 my-2 items-center">
                     <Link href={"/"}>
-                        <a className="w-20 h-12">
-                            <div className="bg-nav-logo bg-contain w-full h-full bg-center bg-no-repeat ">
-                            </div>
+                        <a className="group outline-none active:scale-90 disabled:opacity-50 hover:opacity-80 ">
+                            <div className="group-focus-visible:bg-filecoin-saturn-border bg-contain bg-no-repeat bg-center p-4">
+                               <div className="w-20 h-12 bg-nav-logo bg-contain bg-center bg-no-repeat ">
+                                </div> 
+                            </div> 
                         </a>
                     </Link>
                     <button className="md:hidden h-7 w-7 " onClick={() => {setIsOpen(true)}}>
-                        <div className="bg-nav-menu bg-contain w-full h-full bg-center bg-no-repeat">
+                        <div className="bg-nav-menu bg-contain w-full h-full bg-center bg-no-repeat hover:opacity-80 outline-none disabled:opacity-50">
                         </div>
                     </button>
                         <div className="hidden md:flex space-x-16 mr-10">
@@ -35,8 +61,8 @@ export default function NavBar() {
                             return (
                                 <>
                                     <Link href={link.href} key={index}>
-                                        <a className="">
-                                            <div className="relative text-sm">
+                                        <a className="group outline-none active:scale-90 disabled:opacity-50 hover:opacity-70 group-focus-visible:bg-filecoin-saturn-border bg-contain bg-no-repeat bg-center">
+                                            <div className="relative text-sm ">
                                                 <div className={`font-roboto font-bold tracking-wide ${path.asPath === link.href ? ``: `invisible`}` }>
                                                     {link.title}
                                                 </div>
