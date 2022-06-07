9891522e0fe02e17fda33989acfc92b99e9dcc6a
Nadiem <nadiem8@msn.com>
2022-06-07 14:35:15 +0200

    - Update Backdrop blur feature detection reference #27

--- components/Button8/Button8.tsx
+++ components/Button8/Button8.tsx
@@ -14,7 +14,7 @@ function Button8({text, link, backgroundImage, backdropBlur}: Props) {
     return (
         <Link href={link}>
             <a href="" target="_blank" className="outline-none group " >
-                <div className={`flex items-center space-x-2 rounded-full p-1 px-3 border-2 border-transparent group-focus-visible:border-white group-active:scale-90 will-change-transform ${backdropBlur ? `group-hover:bg-white/25 group-active:bg-white/40 bg-white/10 backdrop-blur-md` : `bg-sat-fallback-blue-2 group-hover:bg-sat-fallback-grey-1/90 group-active:bg-sat-fallback-grey-2/90 group-focus-visible:bg-sat-fallback-grey-1`} `}>
+                <div className={`flex items-center space-x-2 rounded-full p-1 px-3 border-2 border-transparent group-focus-visible:border-white group-active:scale-90 will-change-transform ${backdropBlur ? `supports-blur:group-hover:bg-white/25 supports-blur:group-active:bg-white/40 supports-blur:bg-white/10 supports-blur:backdrop-blur-md bg-sat-fallback-blue-2 group-hover:bg-sat-fallback-grey-1/90 group-active:bg-sat-fallback-grey-2/90 group-focus-visible:bg-sat-fallback-grey-1` : `bg-sat-fallback-blue-2 group-hover:bg-sat-fallback-grey-1/90 group-active:bg-sat-fallback-grey-2/90 group-focus-visible:bg-sat-fallback-grey-1`} `}>
                     <div className="bg-no-repeat bg-contain bg-center h-5 w-5 " style={{backgroundImage: backgroundImage}}> 
                     </div>
                     <div className="text-white text-center font-semibold text-sm lg:text-base font-inter ">
