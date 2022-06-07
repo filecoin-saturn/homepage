f8b7c9f15dcd33ce9b5f3180452f9891414896c1
Nadiem <nadiem8@msn.com>
2022-06-07 17:36:33 +0200

    - Add a dark gradient over star background Reference #33

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,6 +52,7 @@ const Home: NextPage = () => {
     </Head>
     <div className='mx-auto text-center w-full overflow-hidden'>
       <div data-gsap="bg" className='fixed -z-20 inset-x-0 top-0 h-[150%] inset-0 bg-star-background-plain bg-no-repeat bg-cover bg-center'>
+      <div className='bg-gradient-to-b from-transparent to-black w-full h-full'></div>
       </div>
       <div className='fixed -z-10 inset-0'>
         <Experience />
