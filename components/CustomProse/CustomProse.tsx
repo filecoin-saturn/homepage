5d3ba9069b578bbe4b540ba2a58fcb8a53fc8347
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-06-17 15:26:59 +0200

    Make links style and fix it
    Closes #87.

--- components/CustomProse/CustomProse.tsx
+++ components/CustomProse/CustomProse.tsx
@@ -11,7 +11,7 @@ export default function CustomProse({children, overrides}: Props) {
             prose-h3:font-inter prose-h3:font-black prose-h3:text-white prose-h3:text-xl prose-h3:mb-2  prose-h3:md:text-2xl prose-h3:lg:text-[1.7rem] prose-h3:leading-tight
             prose-h4:font-inter prose-h4:font-normal prose-h4:text-white prose-h4:mb-2 prose-h4:text-lg prose-h4:md:text-xl prose-h4:lg:text-base prose-h4:tracking-wide
             prose-p:font-inter prose-p:text-white prose-p:text-lg prose-p:font-normal prose-p:lg:text-base
-            prose-a:text-sat-green-1 prose-a:underline-offset-2 prose-a:font-normal prose-a:font-source-serif-pro
+            prose-a:text-white prose-a:underline-offset-2 prose-a:font-bold prose-a:font-inter
             prose-code:text-white prose-code:rounded-full
             prose-ol:font-source-serif-pro prose-ol:text-white prose-ol:text-lg prose-ol:font-normal prose-ol:lg:text-lg
         ">
