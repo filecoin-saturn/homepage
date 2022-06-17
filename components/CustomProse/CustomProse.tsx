3aeac194f5cb8a6e7b35ccb09a6ac44ae87a49e7
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-06-17 17:24:45 +0200

    Add focus and hover state to prose links

--- components/CustomProse/CustomProse.tsx
+++ components/CustomProse/CustomProse.tsx
@@ -11,7 +11,7 @@ export default function CustomProse({children, overrides}: Props) {
             prose-h3:font-inter prose-h3:font-black prose-h3:text-white prose-h3:mb-1  prose-h3:text-2xl prose-h3:lg:text-[1.7rem] prose-h3:leading-tight
             prose-h4:font-inter prose-h4:font-normal prose-h4:text-white prose-h4:mb-2 prose-h4:text-lg prose-h4:md:text-xl prose-h4:lg:text-base prose-h4:tracking-wide
             prose-p:font-inter prose-p:text-white prose-p:text-lg prose-p:font-normal prose-p:lg:text-base
-            prose-a:text-white prose-a:underline-offset-2 prose-a:font-bold prose-a:font-inter
+            prose-a:text-white prose-a:underline-offset-2 prose-a:font-bold prose-a:font-inter hover:prose-a:underline-offset-[3px] focus:prose-a:text-sat-blue-1 focus:prose-a:outline-none
             prose-ol:font-inter prose-ol:text-white prose-ol:text-lg prose-ol:font-normal prose-ol:lg:text-base marker:prose-ol:text-white marker:prose-ol:font-bold
             prose-ul:font-inter prose-ul:text-white prose-ul:text-lg prose-ul:font-normal prose-ul:lg:text-base prose-ul:my-0 marker:prose-ul:text-white marker:prose-ul:font-bold
             prose-code:after:hidden prose-code:before:hidden prose-code:bg-white prose-code:bg-opacity-20 prose-code:px-1 prose-code:py-0.5 prose-code:mx-0.5 prose-code:rounded-md prose-code:text-white
