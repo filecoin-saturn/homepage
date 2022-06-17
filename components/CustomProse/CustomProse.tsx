f1ecba75009f67715cd3894d7057695e7d90bfdf
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-06-17 15:10:07 +0200

    Change paragraph font

--- components/CustomProse/CustomProse.tsx
+++ components/CustomProse/CustomProse.tsx
@@ -9,8 +9,8 @@ export default function CustomProse({children, overrides}: Props) {
             prose-h1:font-inter prose-h1:font-black prose-h1:my-4 prose-h1:text-white prose-h1:text-5xl prose-h1:md:text-6xl prose-h1:lg:text-[5rem]
             prose-h2:font-inter prose-h2:font-normal prose-h2:text-2xl prose-h2:lg:text-xl prose-h2:text-white prose-h2:my-4
             prose-h3:font-inter prose-h3:font-black prose-h3:text-white prose-h3:text-xl prose-h3:mb-2  prose-h3:md:text-2xl prose-h3:lg:text-[1.7rem] prose-h3:leading-tight
-            prose-h4:font-inter prose-h4:font-normal prose-h4:text-white prose-h4:mb-2 prose-h4:text-lg prose-h4:md:text-xl prose-h4:lg:text-xl prose-h4:tracking-wide
-            prose-p:font-source-serif-pro prose-p:text-white prose-p:text-lg prose-p:font-normal prose-p:lg:text-lg
+            prose-h4:font-inter prose-h4:font-normal prose-h4:text-white prose-h4:mb-2 prose-h4:text-lg prose-h4:md:text-xl prose-h4:lg:text-base prose-h4:tracking-wide
+            prose-p:font-inter prose-p:text-white prose-p:text-lg prose-p:font-normal prose-p:lg:text-base
             prose-a:text-sat-green-1 prose-a:underline-offset-2 prose-a:font-normal prose-a:font-source-serif-pro
             prose-code:text-white prose-code:rounded-full
             prose-ol:font-source-serif-pro prose-ol:text-white prose-ol:text-lg prose-ol:font-normal prose-ol:lg:text-lg
--- content/en/index/Features.tsx
+++ content/en/index/Features.tsx
@@ -2,22 +2,22 @@ import FeaturesTemplate from "../../../components/Features/Features"
 
     const content = [{
         image: "url(permissionless-logo.svg)",
-        title: "1. Serve Content",
+        title: "Serve Content",
         text: "Serve content stored on Filecoin with low latency.",
     },
     {
         image: "url(content-addressed-logo.svg)",
-        title: "2. Accelerate content",
+        title: "Accelerate content",
         text: "Accelerate the adoption of Web3 by accelerating Web3 content, from NFTs to the Metaverse.",
     },
     {
         image: "url(crypto-logo.svg)",
-        title: "3. Everyone can join",
+        title: "Everyone can join",
         text: "Provide a new, easier way for anyone to join the Filecoin network by lowering the hardware requirements to participate. Download a Filecoin app on your desktop and earn Filecoin.",
     },
     {
         image: "url(multi-peer-logo.svg)",
-        title: "4. Multi-peer retrievals",
+        title: "Multi-peer retrievals",
         text: "Unite a heterogeneous network of hardware -- from big servers in datacenters to end user desktops and phones -- into a fast, low-cost CDN.",
     },
   ]
