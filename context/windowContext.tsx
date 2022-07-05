a272c24759204b999fc2e695b9ae8c9cc7f082f9
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-07-05 18:36:39 +0200

    Measure url bar on mobile browser with js
    This references #114.

--- context/windowContext.tsx
+++ context/windowContext.tsx
@@ -4,7 +4,8 @@ import { debounce } from 'lodash';
 const dimDefault = {
     height: 0,
     width: 0,
-    scrollBarWidth: 0
+    scrollBarWidth: 0,
+    urlBarHeight: 0
 }
 
 const WindowContext = createContext(dimDefault)
@@ -23,8 +24,10 @@ export function WindowContextWrapper({children, debounceMs, setVh}: WindowContex
                 setDim({
                     height: window.innerHeight,
                     width: window.innerWidth,
-                    scrollBarWidth: window.innerWidth - document.documentElement.clientWidth
+                    scrollBarWidth: window.innerWidth - document.documentElement.clientWidth,
+                    urlBarHeight: dim.urlBarHeight
                 })
+
             }
         }
         const debouncedHandleWindowResize = debounce(handleResize, debounceMs)
@@ -36,12 +39,28 @@ export function WindowContextWrapper({children, debounceMs, setVh}: WindowContex
     }, [debounceMs])
 
     useEffect(() => {
+        // measure url bar height
+        // on page load quickly scroll down and up again
         if(typeof window !== undefined) {
-            if(setVh) {
-                const vh = window.innerHeight * 0.01
-                document.documentElement.style.setProperty('--vh', `${vh}px`);
-            }
+            // Save current scroll position
+            const oldScrollX = window.scrollX
+            const oldScrollY = window.scrollY
+            // Hide url bar
+            window.scrollTo(0, document.body.scrollHeight)
+            // Measure max viewport height
+            const maxH = window.innerHeight
+            // Scroll back
+            window.scrollTo(oldScrollX, oldScrollY)
+            // Calculate url bar height
+            console.log(maxH - window.innerHeight)
+            setDim({
+                height: dim.height,
+                width: dim.width,
+                scrollBarWidth: dim.scrollBarWidth,
+                urlBarHeight: maxH - window.innerHeight
+            })
         }
+        // scenario: viewport size changes. Directly after that we can measure the scroll position. When the scroll position changed we know that the addres bar hid / showed. 
     }, [])
 
     return (
