1bf2fbb12cc7d041707a7f4b15455d7f83b4c924
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-07-07 15:40:52 +0200

    Fix --vh calculation
    This closes #114.

--- context/windowContext.tsx
+++ context/windowContext.tsx
@@ -15,11 +15,6 @@ type WindowContextWrapperProps = {
     setVh?: boolean;
 }
 
-// Assumptions:
-// 1. Resize handler is debounced in such a way, that the url bar hide/show in mobile browsers only AFTER each action is complete
-// 2. Scroll event is not triggered by orientation change or resize event (i.e. scroll position is maintained when the user resizes the window).
-// 3. Url bar height does not change after the page is loaded, e.g. during orientation changes
-
 export function WindowContextWrapper({children, debounceMs, setVh}: WindowContextWrapperProps) {
     const [dim, setDim] = useState(dimDefault)
     const [urlBarHeight, setUrlBarHeight] = useState(0)
@@ -45,20 +40,22 @@ export function WindowContextWrapper({children, debounceMs, setVh}: WindowContex
                 const h = window.innerHeight
                 const w = window.innerWidth
                 const cw = document.documentElement.clientWidth
-                const isUrlBarNowHidden = controlHeight && controlHeight === h ? true : false
-                const newUrlBarHeight = controlHeight && isUrlBarNowHidden ? controlHeight - h : urlBarHeight
-                const heightWithoutUrlBar = controlHeight && isUrlBarNowHidden ? h - urlBarHeight : h
                 setDim({
                     height: h,
                     width: w,
                     scrollBarWidth: w - cw
                 })
-                setUrlBarHeight(newUrlBarHeight)
-                if(setVh) {
-                    console.log("setting vh to " + heightWithoutUrlBar * 0.01)
-                    const vh = heightWithoutUrlBar * 0.01
-                    document.documentElement.style.setProperty('--vh', `${vh}px`);
+                if(controlHeight) {
+                    const isUrlBarNowHidden = controlHeight === h
+                    const newUrlBarHeight = isUrlBarNowHidden ? urlBarHeight : controlHeight - h
+                    const heightWithoutUrlBar = controlHeight - newUrlBarHeight
+                    setUrlBarHeight(newUrlBarHeight)
+                    if(setVh) {
+                        const vh = heightWithoutUrlBar * 0.01
+                        document.documentElement.style.setProperty('--vh', `${vh}px`);
+                    }
                 }
+                
             }
         }
         const debouncedHandleWindowResize = debounce(handleResize, debounceMs, {trailing:true})
