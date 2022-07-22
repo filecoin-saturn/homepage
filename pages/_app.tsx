fb3511816d95a5498a0b5bef809af99ff7dab43c
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-07-22 20:08:13 +0200

    Remove posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,9 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { WindowContextWrapper } from '../context/windowContext'
 import { FeatureContextWrapper } from '../context/featureContext'
-import { usePostHog } from '../utils/usePostHog'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  usePostHog('phc_RJEgcm0ciIXv09UhIYgC3pZcufMIIhPEHCAUmO5E1YI', { api_host: 'https://app.posthog.com' })
   return (
     <FeatureContextWrapper>
       <WindowContextWrapper debounceMs={60} setVh>
