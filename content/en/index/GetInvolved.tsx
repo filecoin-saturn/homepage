1a35f2d1eaa9ca1e6e2a2b5557525c2f0db94fba
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-07-23 02:18:58 +0200

    Change button order in contact section

--- content/en/index/GetInvolved.tsx
+++ content/en/index/GetInvolved.tsx
@@ -9,8 +9,8 @@ type CallToActionButtonContent ={
 export default function CallToActionButton({backdropBlur}: CallToActionButtonContent) {
     return (
         <>
-            <Button1 text="Get in touch" link='mailto:ansgar@protocol.ai' />
-            <Button2 target="_blank" text="Jobs" link='https://pl-strflt.notion.site/Available-Roles-2eeb7b1ec87e4fdc959d7177acf71771' backdropBlur={backdropBlur} />
+            <Button1 target="_blank" text="Join the team" link='https://pl-strflt.notion.site/Available-Roles-2eeb7b1ec87e4fdc959d7177acf71771' />
+            <Button2 text="Get in touch" link='mailto:ansgar@protocol.ai' backdropBlur={backdropBlur} />
         </>
     )
 }
