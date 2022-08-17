0fff2c2e02ed28fde19eef8d39769c871177b411
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-08-17 11:27:46 +0200

    Minor style fixes

--- components/ListBigDots/ListBigDots.tsx
+++ components/ListBigDots/ListBigDots.tsx
@@ -11,10 +11,10 @@ export default function ListBigDots({children, backdropBlur}: Props) {
             {children.map((child, i, array) => {
                 return (
                     <div key={i} className='flex space-x-1 md:space-x-3'>
-                        <div className='flex flex-col'>
-                            <div className={`${backdropBlur ? ` supports-blur:backdrop-blur-md supports-blur:outline-sat-blue-3/30` : ``} outline-sat-blue-3-30-fallback-1 bg-sat-blue-3 outline-4 outline rounded-full min-w-[1.2rem] min-h-[1.2rem] `}/>
+                        <div className='flex flex-col items-center'>
+                            <div className={`${backdropBlur ? ` supports-blur:backdrop-blur-md supports-blur:outline-sat-blue-3/30` : ``} outline-sat-blue-3-30-fallback-1 bg-sat-blue-3 outline-4 outline-offset-0 outline-none rounded-full min-w-[1.2rem] min-h-[1.2rem] `}/>
                             {i === array.length - 1 ? null : (
-                                <div className={`${backdropBlur ? `supports-blur:backdrop-blur-md supports-blur:border-r-sat-blue-3/30` : `border-r-sat-blue-3-30-fallback-1`} flex-grow my-1 w-1 opacity-30 border-r-2 ml-[0.4rem]`}/>
+                                <div className={`${backdropBlur ? `supports-blur:backdrop-blur-md supports-blur:bg-sat-blue-3/30` : ``} bg-sat-blue-3-30-fallback-1 flex-grow my-2 w-0.5`}/>
                             )}
                         </div>
                         <div className='pl-4 mb-8 -mt-4 md:flex-none max-w-lg'>
