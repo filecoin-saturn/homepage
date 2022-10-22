62065c26fd7b37a72ae284a6444093a8c4357910
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-10-22 19:21:40 +0200

    Make player autoplay

--- components/AsciinemaPlayer/AsciinemaPlayer.tsx
+++ components/AsciinemaPlayer/AsciinemaPlayer.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import 'asciinema-player/dist/bundle/asciinema-player.css';
 import { useWindowContext } from '../../context/windowContext';
+import {cloneDeep} from 'lodash'
 
 type AsciinemaPlayerProps = {
     src: string;
@@ -18,21 +19,27 @@ type AsciinemaPlayerProps = {
     poster?: string;
     fit?: string;
     fontSize?: string;
+    play?: boolean;
     // END asciinemaOptions
 };
 
-function AsciinemaPlayer ({src, className, ...asciinemaOptions}: AsciinemaPlayerProps) {
+function AsciinemaPlayer ({src, className, play, ...asciinemaOptions}: AsciinemaPlayerProps) {
+    console.log(play)
     const ref = useRef<HTMLDivElement>(null);
     const [player, setPlayer] = useState<typeof import("asciinema-player")>()
+    const [instance, setInstance] = useState<Record<string, unknown> & {dispose: () => void, play: () => void, pause: () => void, seek: (number: number) => void}>()
     const windowContext = useWindowContext()
     useEffect(() => {
         import("asciinema-player").then(p => {setPlayer(p)})
+        return () => {setPlayer(undefined)}
     }, [])
     useEffect(() => {
         const currentRef = ref.current
-        const instance = player?.create(src, currentRef, asciinemaOptions);
-        return () => {instance?.dispose()}
-    }, [src, player, asciinemaOptions, windowContext.width]);
+        const i = play ? player?.create(src, currentRef, asciinemaOptions) : undefined
+        return () => {
+            i?.dispose()
+        }
+    }, [src, player, asciinemaOptions, windowContext.width, play]);
 
     return <div className={className} ref={ref} />;
 }
