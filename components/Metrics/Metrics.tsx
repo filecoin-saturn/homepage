bd8c05b2a1742974c0fdfeca90b9c16ba500dd23
Nadiem <nadiem8@msn.com>
2022-10-24 12:23:54 +0200

    small metrics fix

--- components/Metrics/Metrics.tsx
+++ components/Metrics/Metrics.tsx
@@ -36,7 +36,7 @@ export default function Metrics({contentId}: Props) {
                 return (
                     <div key={key} className="first-of-type:mr-4  text-white font-inter min-w-fit flex items-center space-x-1 md:space-x-3 lg:space-x-4 ">
                         <div className="text-[1.25rem] md:text-4xl lg:text-5xl font-black" >
-                            {[pulledMetrics[key]?? postContent.number]}{postContent.unit}
+                            {pulledMetrics[key]?? postContent.number}{postContent.unit}
                         </div>
                         <div className="text-[0.625rem] max-w-[6rem] md:max-w-[7rem] md:text-sm lg:text-lg lg:max-w-[9rem] leading-none md:leading-4 lg:leading-5 whitespace-pre-wrap">
                             {postContent.metric}
