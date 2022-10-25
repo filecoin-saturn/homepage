f18aa053ce304bc8013c26094f256d5d0f78e424
Nadiem <nadiem8@msn.com>
2022-10-25 11:12:51 +0200

    Update metrics fetch request to updated content

--- dataFetching/dataFetching.tsx
+++ dataFetching/dataFetching.tsx
@@ -5,7 +5,7 @@ export async function getUptoDateMetrics(content : Array<{number: number}>) {
     const response = await fetch('https://uc2x7t32m6qmbscsljxoauwoae0yeipw.lambda-url.us-west-2.on.aws/?filAddress=all&startDate=1662596100000&endDate=1663200900000&step=day', options)
     const jsonResponse = await response.json()
     if(jsonResponse) {
-        const pops = jsonResponse.nodes ? jsonResponse.nodes[0].count + jsonResponse.nodes[1].count : content[0].number
+        const pops = jsonResponse.nodes ? jsonResponse.nodes[0].count : content[0].number
         const numRequestsServed = (jsonResponse.metrics[0].numRequests / 1000000) > 100 ? jsonResponse.metrics[0].numRequests / 1000000 : content[1].number
         return [Number(pops.toFixed()), Number(numRequestsServed.toFixed())]
     }else {
