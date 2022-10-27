6d53f216fbb04efa670cb8dabc280f6021f60f67
Benjamin Preiss <benjaminpreiss@protonmail.com>
2022-10-27 15:05:33 +0100

    Change plausible tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,29 +3,70 @@ import type { AppProps } from 'next/app'
 import WindowContextWrapper from '../context/windowContext'
 import FeatureContextWrapper from '../context/featureContext'
 import Script from 'next/script'
+import Head from 'next/head'
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
 <>
-    <Script 
-    async
-    strategy='afterInteractive' 
-    src='https://www.googletagmanager.com/gtag/js?id=G-DFGH1FW5NT' 
-    />
-    <Script strategy="afterInteractive" id='google-analytics'>
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-DFGH1FW5NT', {
-            page_path: window.location.pathname,
-            });
-        `}
-    </Script> 
-    <Script defer data-domain="strn.network" src="https://plausible.io/js/plausible.js"/>
+    {typeof window !== 'undefined' &&
+      window.location.hostname.includes("strn.network") && (
+        <>
+          <Script 
+            async
+            strategy='afterInteractive' 
+            src='https://www.googletagmanager.com/gtag/js?id=G-DFGH1FW5NT' 
+          />
+          <Script strategy="afterInteractive" id='google-analytics'>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DFGH1FW5NT', {
+              page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+          <Script strategy='afterInteractive' async data-domain="strn.network" src="https://plausible.io/js/plausible.js"/>
+          <Script strategy='afterInteractive' async id='plausible-analytics'>
+            {'window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }'}
+          </Script>
+          {/* Send click events on data-analytics links to plausible*/}
+          <Script async strategy='afterInteractive' id='plausible-analytics-anchor-script'>
+            {`
+              let links = document.querySelectorAll("a[data-analytics]");
+              for (var i = 0; i < links.length; i++) {
+                  links[i].addEventListener('click', handleLinkEvent);
+                  links[i].addEventListener('auxclick', handleLinkEvent);
+              }
 
+              function handleLinkEvent(event) {
+                  var link = event.target;
+                  var middle = event.type == "auxclick" && event.which == 2;
+                  var click = event.type == "click";
+                  while (link && (typeof link.tagName == 'undefined' || link.tagName.toLowerCase() != 'a' || !link.href)) {
+                      link = link.parentNode;
+                  }
+                  if (middle || click) {
+                      let attributes = link.getAttribute('data-analytics').split(/,(.+)/);
+                      let events = [JSON.parse(attributes[0]), JSON.parse(attributes[1] || '{}')];
+                      plausible(...events);
+                  }
+                  if (!link.target) {
+                      if (!(event.ctrlKey || event.metaKey || event.shiftKey) && click) {
+                          setTimeout(function () {
+                              location.href = link.href;
+                          }, 150);
+                          event.preventDefault();
+                      }
+                  }
+              }
+            `}
+          </Script>
+        </>
+      )
+    }
     <FeatureContextWrapper>
       <WindowContextWrapper debounceMs={60} setVh>
         <Component {...pageProps} />
