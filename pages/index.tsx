484c22455df3b4a458952ea751685e9d8c2dbfa4
Nadiem <nadiem8@msn.com>
2022-10-29 11:07:56 +0100

    Change meta-title and update fallback states from metrics

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Filecoin Saturn</title>
+        <title>Filecoin Saturn | The Web3 CDN</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
