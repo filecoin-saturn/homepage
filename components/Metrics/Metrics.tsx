461b91e7cd33cc56d3803c60f60df04e56b5f698
Nadiem <nadiem8@msn.com>
2022-10-31 13:47:04 +0100

    Fix small metrics layout design issue

--- components/Metrics/Metrics.tsx
+++ components/Metrics/Metrics.tsx
@@ -45,10 +45,10 @@ export default function Metrics({contentId}: Props) {
         })
         
     return (
-        <div className="flex flex-wrap w-full first-of-type:mr-4 xs:gap-3  md:gap-8 lg:gap-16 max-w-none md:my-8 xl:gap-16 ">
+        <div className="flex flex-wrap w-full first-of-type:mr-4 xs:gap-3  md:gap-8 lg:gap-12 max-w-none md:my-8 xl:gap-14 ">
             {content.map((postContent: PostContentContent, key: number) => {
                 return (
-                    <div key={key} className="first-of-type:mr-4  text-white font-inter min-w-fit flex items-center space-x-1 md:space-x-3 lg:space-x-4 ">
+                    <div key={key} className="first-of-type:mr-4  text-white font-inter min-w-fit flex items-center space-x-1 md:space-x-3 lg:space-x-3 ">
                         <div className="text-[1.25rem] md:text-4xl lg:text-5xl font-black flex space-x-0.5 md:space-x-1 lg:space-x-2">
                             <animated.div className={`${content[key].number.length === 2 ? `w-7 md:w-12 lg:w-[65px]` : `w-10 md:w-[70px] lg:w-[90px]` }`} >
                                 {key === 0 ? styles.x.to(val => Math.floor(val)) : key === 1 ? styles.y.to(val => Math.floor(val)) : styles.z.to(val => Math.floor(val))}  
