62f61abfcb821ff7088a577c4bf736508bd5a3c7
Nadiem <nadiem8@msn.com>
2022-11-15 10:47:52 +0100

    test subpages on fleek

---
+++ pages/test.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+
+
+const Test: NextPage = () => {
+  
+  return (
+   <div className='text-white w-full'>
+        testing if it works on fleek
+   </div>
+  )
+}
+export default Test
