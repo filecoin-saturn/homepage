aa834abf93a05726c849662500ed89a139975af0
Jay <jennifer.brawn@gmail.com>
2025-01-07 12:19:49 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -115,10 +115,10 @@ const Home: NextPage = () => {
               className="font- max-w-sm xs:max-w-md sm:max-w-md md:max-w-4xl lg:max-w-7xl  mr-auto relative"
             >
               <h1 className="text-white font-inter text-5xl lg:text-6xl font-black mt-4 mb-8">
-                Filecoin Saturn revolutionized Web3 content delivery, but now there's a new kid on the block. 
+                Filecoin Saturn revolutionized Web3 content delivery, but now there's a new kid on the block - meet <a href="https://storacha.network/" className="hover:underline font-inter font-semibold text-orange-500">Storacha</a>! 
               </h1>
               <p className="text-slate-400 text-xl lg:text-2xl">
-                Check out <a href="https://storacha.network/" className="hover:underline font-inter font-semibold text-orange-500">Storacha</a> for hotter and more decentralized storage!
+                Check out Storacha for hotter and more decentralized storage!
               </p>
               <div className="absolute -inset-x-[110%] -inset-y-[60%] -z-10 opacity-50 bg-gradient-radial from-black via-transparent to-transparent bg-cover"></div>
             </div>
